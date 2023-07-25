f5a63b38f715fc4a1a9326cdbbc9c462d411cfdd
Xhelat <aleksjoke98@gmail.com>
2023-07-25 21:16:11 +0300

    add unique keywords of emoji

--- src/script/script.js
+++ src/script/script.js
@@ -3,33 +3,47 @@ import { data } from "../data/emoji.js";
 const input = document.querySelector(".header__input");
 const cardsWrapper = document.querySelector(".cards");
 
+const uniqueData = getUniqueData();
+
+function getUniqueData() {
+    return data.map((emojiObject) => {
+        const uniqueKeywords = [
+            ...new Set(emojiObject.keywords.split(" ")),
+        ].join(" ");
+        return { ...emojiObject, keywords: uniqueKeywords };
+    });
+}
+
 input.addEventListener("input", (evt) => {
     const inputValue = evt.target.value.toLowerCase().trim();
 
-    const filteredData = data.filter(
-        (emojiObject) =>
-            emojiObject.title.toLowerCase().trim().includes(inputValue) ||
-            emojiObject.keywords.toLowerCase().trim().includes(inputValue)
-    );
+    const filteredData = uniqueData.filter((emojiObject) => {
+        const lowerTitle = emojiObject.title.toLowerCase().trim();
+        const lowerKeywords = emojiObject.keywords.toLowerCase().trim();
+        return (
+            lowerTitle.includes(inputValue) ||
+            lowerKeywords.includes(inputValue)
+        );
+    });
 
-    renderCards(filteredData)
-})
+    renderCards(filteredData);
+});
 
-function createCard(obj) {
+function createCard({ symbol, title, keywords }) {
     const card = document.createElement("div");
     card.className = "cards__item";
-    card.innerHTML = `<p class="cards__item_icon">${obj.symbol}</p>
-                      <p class="cards__item_title">${obj.title}</p>
-                      <p class="cards__item_descr">${obj.keywords}</p>`;
+    card.innerHTML = `<p class="cards__item_icon">${symbol}</p>
+                      <p class="cards__item_title">${title}</p>
+                      <p class="cards__item_descr">${keywords}</p>`;
     return card;
 }
 
 function renderCards(dataArray) {
-    cardsWrapper.innerHTML = ''
+    cardsWrapper.innerHTML = "";
     dataArray.forEach((emojiObject) => {
-        const card = createCard(emojiObject)
+        const card = createCard(emojiObject);
         cardsWrapper.appendChild(card);
     });
 }
 
-renderCards(data)
+renderCards(uniqueData);
